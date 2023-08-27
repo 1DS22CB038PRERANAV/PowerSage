efc63019f6a73acb78c1ff2214ac99846a618447
1DS22CB038PRERANAV <136422031+1DS22CB038PRERANAV@users.noreply.github.com>
2023-08-27 12:17:36 +0530

    Added Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Image } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-dark text-light text-center py-4">
+      <div className="container d-flex justify-content-between">
+        <div className="footer-left">
+          <Image
+            alt=""
+            src="/PowerSageLogo.png"
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />
+          &nbsp;
+          <h4 className="mb-0"> Power Sage</h4>
+        </div>
+        <div className="footer-right">
+          <h4 className="mb-2">
+            Dayananda Sagar College of Engineering, Bengaluru
+          </h4>
+          <h5 className="mb-0 mt-4">Team Members:</h5>
+          <ul className="mb-0">
+            <li>Prerana V</li>
+            <li>Priyanshu Kumar Sinha</li>
+            <li>Priyanshu Pratik</li>
+            <li>Prasidh Krishna</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
+import Footer from "@/components/Footer";
 import NavbarComponent from "@/components/NavbarComponent";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import { AuthContextProvider } from "@/context/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
 import { useRouter } from "next/router";
+import { Container } from "react-bootstrap";
 
 const noAuthRequired = ["/", "/login", "/signup"];
 export default function App({ Component, pageProps }) {
@@ -10,13 +13,21 @@ export default function App({ Component, pageProps }) {
   return (
     <AuthContextProvider>
       <NavbarComponent />
-      {noAuthRequired.includes(router.pathname) ? (
-        <Component {...pageProps} />
-      ) : (
-        <ProtectedRoute>
+      <Container
+        style={{
+          minHeight: "75vh",
+        }}
+        fluid
+      >
+        {noAuthRequired.includes(router.pathname) ? (
           <Component {...pageProps} />
-        </ProtectedRoute>
-      )}
+        ) : (
+          <ProtectedRoute>
+            <Component {...pageProps} />
+          </ProtectedRoute>
+        )}
+      </Container>
+      <Footer />
     </AuthContextProvider>
   );
 }
